36e44a79e8cbdbeacd953cfec918fc000e70bd79
MaximeElKhatib <143719157+MaximeElKhatib@users.noreply.github.com>
2024-12-10 01:20:19 -0500

    Delete projet-kmmd directory

--- projet-kmmd/backend/routes/employerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const employerController = require("../controllers/employerController");
-
-router.get("/", employerController.getEmployers);
-router.get("/:eid", employerController.getEmployerById);
-router.post("/login", employerController.loginEmployeur);
-router.post("/signup", employerController.signUpEmployeur);
-module.exports = router;
